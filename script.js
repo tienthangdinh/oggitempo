1920dbcf795a07b030c1895d48186b52c443e46c
Tien Thang Dinh <tien-thang.dinh@t-systems.com>
2022-09-06 22:25:21 +0200

    first step API

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-const searchButton = document.querySelector("#landingsection > form > button");
-const carousel = document.querySelector("#carousel");
-const landingsection = document.querySelector("#landingsection");
-const navsearchbar = document.querySelector("#subnav > .searchbar");
-
-searchButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  //   carousel.style.transform = `translateY(calc(${-landingsection.offsetHeight}px - 5rem))`;
-  // slideshow approach does not work when the width of landingspace itself changes together when view height changes
-  //problem solved easily
-  landingsection.style.display = "none";
-  navsearchbar.style.visibility = "visible";
-});
+//--------------------Themes Toggle---------------------------
 
 const themeButton = document.getElementById("subthemes");
 const mainnav = document.getElementById("mainnav");
@@ -31,6 +19,8 @@ themeButton.addEventListener("click", () => {
   }
 });
 
+//------------------Mobile Hamburger-------------------
+
 const hamburger = document.getElementById("hamburger");
 const collapse = document.getElementById("collapse");
 
@@ -38,3 +28,51 @@ hamburger.addEventListener("click", () => {
   let top = collapse.style.top;
   collapse.style.top = top == "-100%" ? "3.2rem" : "-100%";
 });
+
+//-------------------------API---------------------------
+
+//-----------input city---------------
+const listofsearchbars = document.getElementsByTagName("form");
+const searchbar0 = document.querySelector("#subnav > form > input[type=text]");
+const searchbar1 = document.querySelector(
+  "#landingsection > form > input[type=text]"
+);
+const carousel = document.querySelector("#carousel");
+const landingsection = document.querySelector("#landingsection");
+const navsearchbar = document.querySelector("#subnav > .searchbar");
+
+listofsearchbars[1].addEventListener("submit", (e) => {
+  e.preventDefault();
+  landingsection.style.display = "none";
+  navsearchbar.style.visibility = "visible";
+  const city = searchbar1.value;
+  if (city) {
+    getWeather(city);
+  }
+});
+
+listofsearchbars[0].addEventListener("submit", (e) => {
+  e.preventDefault();
+  const city = searchbar0.value;
+  if (city) {
+    getWeather(city);
+  }
+});
+
+//-----------fetch weather info------------
+
+const apikey = "bb27bb9276e62533a17594b47eef09c1";
+const url = (city) =>
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+const KtoC = (K) => Math.floor(K - 273.15);
+
+async function getWeather(city) {
+  try {
+    const response = await fetch(url(city));
+    const result = await response.json();
+    console.log(result);
+    console.log(`${city} ` + KtoC(result.main.temp) + "°C");
+  } catch (error) {
+    console.log("error");
+  }
+}
